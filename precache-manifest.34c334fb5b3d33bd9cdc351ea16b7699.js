609d55b14572bda52a8dc29786588f1b93f25175
TAENY-KIM <kimtaeeeny@naver.com>
2019-11-14 02:03:53 +0900

    Updates

---
+++ precache-manifest.34c334fb5b3d33bd9cdc351ea16b7699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf9494042ced8edb7c00dd082fe9934",
+    "url": "/t_portfolio_v1/index.html"
+  },
+  {
+    "revision": "9162ea8a6900c9c4d647",
+    "url": "/t_portfolio_v1/static/css/main.a9512ba4.chunk.css"
+  },
+  {
+    "revision": "2c640e385856d3cca2ef",
+    "url": "/t_portfolio_v1/static/js/2.b6b56324.chunk.js"
+  },
+  {
+    "revision": "9162ea8a6900c9c4d647",
+    "url": "/t_portfolio_v1/static/js/main.c8568b85.chunk.js"
+  },
+  {
+    "revision": "7f505f9eea51c80ad573",
+    "url": "/t_portfolio_v1/static/js/runtime-main.234942a4.js"
+  }
+]);
