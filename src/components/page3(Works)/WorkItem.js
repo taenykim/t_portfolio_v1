4faedbbb3cfb143ee64630857f4bd96e7f94aef8
TAENY-KIM <kimtaeeeny@naver.com>
2019-11-22 22:30:56 +0900

    work 만드는중

--- src/components/page3(Works)/WorkItem.js
+++ src/components/page3(Works)/WorkItem.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class WorkItem extends Component {
-    render() {
-        return (
-            <div className="workItem_container">
-                <img className="work_image" src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"/>
-                <div className="WorkItem_title">Fahub</div>
-                <div className="WorkItem_subtitle">패션정보 공유 사이트</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="workItem_container">
+        <img
+          className="work_image"
+          src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+        />
+        <div className="WorkItem_title">Fahub</div>
+        <div className="WorkItem_subtitle">패션정보 공유 사이트</div>
+        <br />
+        깃주소, 제작과정노트
+        <br />
+        사용된 기술 넣기
+        <br />
+        <br />
+        <div className="WorkItem_visit">Go to Link</div>
+      </div>
+    );
+  }
 }
 
 export default WorkItem;
