c1caa71a99405c535c4f9fab7ee0fc53a860552b
TAENY-KIM <kimtaeeeny@naver.com>
2019-11-24 21:26:10 +0900

    works 데스크탑 폰 끝

--- src/components/page3(Works)/WorkItem.js
+++ src/components/page3(Works)/WorkItem.js
@@ -1,86 +1,199 @@
 import React, { Component, Fragment } from "react";
 
 class WorkItem extends Component {
-  static defaultProps = {
-    contents:"fashub",
-  };
-  state={
-    title:["Fashub", "portfoliov1", "none"],
-    subtitle:["패션정보 공유 커뮤니티", "포트폴리오 v1", "none"],
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+      desktop: true,
+      title: ["Fashub", "portfoliov1", "none"],
+      subtitle: ["패션정보 공유 커뮤니티", "포트폴리오 v1", "none"]
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    if (window.innerWidth < 1040) {
+      this.setState({
+        desktop: false
+      });
+    } else {
+      this.setState({
+        desktop: true
+      });
+    }
+  }
+
+  static defaultProps = {
+    contents: "fashub"
+  };
+  // state = {};
+
   render() {
     return (
       <Fragment>
-      {this.props.contents==="fashub" && ( <div className="workItem_container">
-        <img
-          className="work_image"
-          src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
-        />
-        <div>
-          <div className="WorkItem_title">{this.state.title[0]}</div>
-          <div className="WorkItem_subtitle">{this.state.subtitle[0]}</div>
-        </div>
+        {this.props.contents === "fashub" && this.state.desktop && (
+          <div className="workItem_container">
+            <img
+              className="work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[0]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[0]}</div>
+            </div>
 
-        <div className="WorkItem_desc">
-          깃주소, 제작과정노트
-          <br />
-          사용된 기술 넣기
-          <br />
-          <br />
-        </div>
-        <div className="WorkItem_footer">
-          <div className="WorkItem_visit">Visit Website</div>
-          <div className="WorkItem_visit">제작과정노트</div>
-        </div>
-      </div>)}
-     
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
 
-      {this.props.contents==="portfoliov1" && ( <div className="workItem_container">
-        <img
-          className="work_image"
-          src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
-        />
-        <div>
-          <div className="WorkItem_title">{this.state.title[1]}</div>
-          <div className="WorkItem_subtitle">{this.state.subtitle[1]}</div>
-        </div>
+        {this.props.contents === "portfoliov1" && this.state.desktop && (
+          <div className="workItem_container">
+            <img
+              className="work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[1]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[1]}</div>
+            </div>
 
-        <div className="WorkItem_desc">
-          깃주소, 제작과정노트
-          <br />
-          사용된 기술 넣기
-          <br />
-          <br />
-        </div>
-        <div className="WorkItem_footer">
-          <div className="WorkItem_visit">Visit Website</div>
-          <div className="WorkItem_visit">제작과정노트</div>
-        </div>
-      </div>)}
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
 
+        {this.props.contents === "none" && this.state.desktop && (
+          <div className="workItem_container">
+            <img
+              className="work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[2]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[2]}</div>
+            </div>
 
-      {this.props.contents==="none" && ( <div className="workItem_container">
-        <img
-          className="work_image"
-          src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
-        />
-        <div>
-          <div className="WorkItem_title">{this.state.title[2]}</div>
-          <div className="WorkItem_subtitle">{this.state.subtitle[2]}</div>
-        </div>
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
 
-        <div className="WorkItem_desc">
-          깃주소, 제작과정노트
-          <br />
-          사용된 기술 넣기
-          <br />
-          <br />
-        </div>
-        <div className="WorkItem_footer">
-          <div className="WorkItem_visit">Visit Website</div>
-          <div className="WorkItem_visit">제작과정노트</div>
-        </div>
-      </div>)}
+{this.props.contents === "fashub" && !this.state.desktop && (
+          <div className="bar_workItem_container">
+            <img
+              className="bar_work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[0]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[0]}</div>
+            </div>
+
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
+
+        {this.props.contents === "portfoliov1" && !this.state.desktop && (
+          <div className="bar_workItem_container">
+            <img
+              className="bar_work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[1]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[1]}</div>
+            </div>
+
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
+
+        {this.props.contents === "none" && !this.state.desktop && (
+          <div className="bar_workItem_container">
+            <img
+              className="bar_work_image"
+              src="https://artfiles.alphacoders.com/609/thumb-60983.jpg"
+            />
+            <div>
+              <div className="WorkItem_title">{this.state.title[2]}</div>
+              <div className="WorkItem_subtitle">{this.state.subtitle[2]}</div>
+            </div>
+
+            <div className="WorkItem_desc">
+              깃주소, 제작과정노트
+              <br />
+              사용된 기술 넣기
+              <br />
+              <br />
+            </div>
+            <div className="WorkItem_footer">
+              <div className="WorkItem_visit">Visit Website</div>
+              <div className="WorkItem_visit">제작과정노트</div>
+            </div>
+          </div>
+        )}
       </Fragment>
     );
   }
--- src/components/page3(Works)/Works.js
+++ src/components/page3(Works)/Works.js
@@ -3,9 +3,9 @@ import "./Works.css";
 import WorkItem from "./WorkItem";
 
 class Works extends Component {
-  state={
-    mode:"fashub",
-  }
+  state = {
+    mode: "fashub"
+  };
   _isMounted = false;
   constructor(props) {
     super(props);
@@ -38,56 +38,75 @@ class Works extends Component {
 
   modeChange1 = () => {
     this.setState({
-      mode: "fashub",
+      mode: "fashub"
     });
-    document.getElementsByClassName("works_button")[0].style="background:white; color:rgba(96,74,138)"
-    document.getElementsByClassName("works_button")[1].style="background:rgba(96,74,138); color:white"
-    document.getElementsByClassName("works_button")[2].style="background:rgba(96,74,138); color:white"
-  }
+    document.getElementsByClassName("works_button")[0].style =
+      "background:white; color:rgba(96,74,138)";
+    document.getElementsByClassName("works_button")[1].style =
+      "background:rgba(96,74,138); color:white";
+    document.getElementsByClassName("works_button")[2].style =
+      "background:rgba(96,74,138); color:white";
+  };
   modeChange2 = () => {
     this.setState({
-      mode: "portfoliov1",
+      mode: "portfoliov1"
     });
-    document.getElementsByClassName("works_button")[0].style="background:rgba(96,74,138); color:white"
-    document.getElementsByClassName("works_button")[1].style="background:white; color:rgba(96,74,138)"
-    document.getElementsByClassName("works_button")[2].style="background:rgba(96,74,138); color:white"
+    document.getElementsByClassName("works_button")[0].style =
+      "background:rgba(96,74,138); color:white";
+    document.getElementsByClassName("works_button")[1].style =
+      "background:white; color:rgba(96,74,138)";
+    document.getElementsByClassName("works_button")[2].style =
+      "background:rgba(96,74,138); color:white";
 
     console.log(document.getElementsByClassName("works_button")[1]);
-  }
+  };
   modeChange3 = () => {
     this.setState({
-      mode: "none",
+      mode: "none"
     });
-    document.getElementsByClassName("works_button")[0].style="background:rgba(96,74,138); color:white"
-    document.getElementsByClassName("works_button")[1].style="background:rgba(96,74,138); color:white"
-    document.getElementsByClassName("works_button")[2].style="background:white; color:rgba(96,74,138)"
-  }
+    document.getElementsByClassName("works_button")[0].style =
+      "background:rgba(96,74,138); color:white";
+    document.getElementsByClassName("works_button")[1].style =
+      "background:rgba(96,74,138); color:white";
+    document.getElementsByClassName("works_button")[2].style =
+      "background:white; color:rgba(96,74,138)";
+  };
 
   render() {
     return (
       <Fragment>
         {this.state.desktop ? (
           <div className="Works_container" id="page3">
-            {/* <div className="Content_title">Works</div> */}
+            <div className="Content_title">Works</div>
             <div className="Content_desc">
-              <div>This is</div>
-              <div>Works</div>
-              <div>Page</div>
+              <WorkItem contents={"fashub"} />
+              <WorkItem contents={"portfoliov1"} />
+              <WorkItem contents={"none"} />
+
             </div>
           </div>
         ) : (
-          <div className="Works_container" id="page3">
+          <div className="bar_Works_container" id="page3">
             <div className="bar_Content_title">Works</div>
             <div className="bar_Content_desc">
               <div className="button_container">
                 <table>
-                  <td className="works_button" onClick={this.modeChange1} style={{background:"white",
-                color:"rgba(96,74,138)"}}>fashub</td>
-                  <td className="works_button" onClick={this.modeChange2}>portfoliov1</td>
-                  <td className="works_button" onClick={this.modeChange3}>none</td>
+                  <td
+                    className="works_button"
+                    onClick={this.modeChange1}
+                    style={{ background: "white", color: "rgba(96,74,138)" }}
+                  >
+                    fashub
+                  </td>
+                  <td className="works_button" onClick={this.modeChange2}>
+                    portfoliov1
+                  </td>
+                  <td className="works_button" onClick={this.modeChange3}>
+                    none
+                  </td>
                 </table>
               </div>
-              <WorkItem contents={this.state.mode}/>
+              <WorkItem contents={this.state.mode} />
             </div>
           </div>
         )}
